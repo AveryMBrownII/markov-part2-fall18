71c96bd52484d0889e69e0f8c7b594081df246b8
Avery Brown <averymbrown22@gmail.com>
2018-10-17 20:47:28 -0400

    passed tests in MarkovTest

---
+++ src/EfficientMarkov.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.NoSuchElementException;
+
+public class EfficientMarkov extends BaseMarkov {
+	public HashMap<String,ArrayList<String>> myMap; // keys are the k-grams in a k-order Markov model.
+													// values are a list of single-character letters that follow the key
+	public EfficientMarkov (int order) {
+		super(order);
+		myMap = new HashMap<>();
+	}
+
+	public EfficientMarkov() {
+		super(3);
+		myMap = new HashMap<>();
+	}
+	
+	@Override
+	public void setTraining(String text) {
+		myText = text;
+		myMap.clear();
+		int index = 0;
+		while (index < myText.length()-myOrder-1){
+			String kgram = myText.substring(index, index+myOrder);
+			myMap.putIfAbsent(kgram, new ArrayList<String>());
+			myMap.get(kgram).add(String.valueOf(myText.charAt(index+myOrder)));
+			index += 1;
+		}
+		String kgram = myText.substring(index, index+myOrder);
+		myMap.putIfAbsent(kgram,new ArrayList<String>());
+		myMap.get(kgram).add(PSEUDO_EOS);	
+	}
+	
+	@Override
+	public ArrayList<String> getFollows(String key){	
+		if (myMap.containsKey(key)) {return myMap.get(key);}
+		else {throw new NoSuchElementException(key+" not in map");}
+
+	}
+}
